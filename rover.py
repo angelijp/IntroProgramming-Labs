9930d1dd563b0b54e9307f53639892f4fd0f142a
Angeli Pinol <Angeli.Pinol1@marist.edu>
2018-09-06 17:26:48 -0400

    Solution to Mars Curiousity Rover Exercise.

---
+++ rover.py
@@ -0,0 +1,18 @@
+# CMPT 120L 113
+# Mars Curiosity Rover
+# Author: Angeli Pinol
+# Date: Sep 6 2018
+
+def main():
+    print("Hello, World")
+    
+
+    d = (34000000)
+
+    s = (186000)
+
+    print( str(d/s) + " seconds ")
+
+main()
+
+

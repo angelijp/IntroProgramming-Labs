98ea9c7f5861e12cf88c88557f4b15ed96731421
Angeli Pinol <Angeli.Pinol1@marist.edu>
2018-09-12 17:26:13 -0400

    Madlibs Program titled, 'A Walk in the Park' by Angeli Pinol

---
+++ madlib.py
@@ -0,0 +1,12 @@
+# Mad Lib Program
+# Author: Angeli Pinol
+
+print("Welcome to Angie's Mad Lib Story!")
+print("MadLibs Story : 'A Walk Through the Park'")
+
+noun = input("Enter a noun: ")
+verb = input("Enter a verb (ending in 'ing': ")
+adjective = input("Enter an adjective: ")
+place = input("Enter a place: ")
+
+print("It is so nice to be with your", noun, verb, "at the", place,"on such a", adjective, "day!") 

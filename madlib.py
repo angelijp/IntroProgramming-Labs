2bce6d726de00e8e815f0626e1d262cd2c8e203a
Angeli Pinol <Angeli.Pinol1@marist.edu>
2018-09-13 15:22:37 -0400

    Made changes to madlib.py by adding three functions (makeAndPrintSentence, promptForWords, and main).

--- madlib.py
+++ madlib.py
@@ -3,10 +3,27 @@
 
 print("Welcome to Angie's Mad Lib Story!")
 print("MadLibs Story : 'A Walk Through the Park'")
+    
+def promptForWords():
+    global noun, verb, adjective, place
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb (ending in 'ing': ")
+    adjective = input("Enter an adjective: ")
+    place = input("Enter a place: ")
+    
+promptForWords()
 
-noun = input("Enter a noun: ")
-verb = input("Enter a verb (ending in 'ing': ")
-adjective = input("Enter an adjective: ")
-place = input("Enter a place: ")
+def makeAndPrintSentence():
+    
+    print("It is so nice to be with your " + noun + verb + " at the " + place + " on such a " + adjective + " day! ")
 
-print("It is so nice to be with your", noun, verb, "at the", place,"on such a", adjective, "day!") 
+makeAndPrintSentence()
+
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+
+main()
+
+    
+    

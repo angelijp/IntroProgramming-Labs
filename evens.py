52f63f27f8cd08b679dd7671ed8203a5cda8c240
Angeli Pinol <Angeli.Pinol1@marist.edu>
2018-09-13 14:12:08 -0400

    Evens Loop Exercise showing range 0 to 20, even numbers only.

--- evens.py
+++ evens.py
@@ -1,12 +1,3 @@
-for i in range(20):
+for i in range(0, 20, 2):
     print(i)
-
-num_list = list(range(2, 22))
-
-even_nums = []
-
-for x in num_list:
-        if x % 2 == 0:
-            even_nums.append(x)
-
-print(even_nums)
+    

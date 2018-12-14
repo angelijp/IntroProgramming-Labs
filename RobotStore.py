4775c37c1d8ed417ef47070e927382ddebf91f93
Angeli Pinol <Angeli.Pinol1@marist.edu>
2018-12-14 16:10:08 -0500

    Modified Robotstore.py using classes and objects.

--- RobotStore.py
+++ RobotStore.py
@@ -3,7 +3,6 @@
 # Prof. Johnson
 # CMPT120L - 113
 # Nov 29 2018
-
 productNames = [ "Ultrasonic range finder"
                , "Servo motor"
                , "Servo controller"
@@ -22,6 +21,39 @@ def printStock():
          if productQuantities[i] > 0:
              print(str(i)+")",productNames[i], "$", productPrices[i])
      print()
+     
+# First define product class using constructor
+
+class Products:
+    def __init__(self,name,price,quantities):
+        self.name = name
+        self.price = price
+        self.quantities = quantities
+    def __str__(self):
+        ans= "Name:    "+self.name
+        ans+="Price:   "+str(self.price)
+        ans+="Quantity:"+str(self.quantities)
+    def  getName(self):
+        return self.name
+    def getPrice(self):
+        return self.price
+    def getQuant(self):
+        return self.quantities
+    def setName(self, newname):
+        self.name = newname
+    def setPrice(self, newprice):
+        self.price = newprice
+    def setQuant(self, newquant):
+        self.quantities = newquant
+        
+# First define product class using constructor
+        product_instances = [Products("Ultrasonic range finder", 2.50,4),
+                             Products("Servo motor",14.99, 10),
+                             Products("Servo controller", 44.95, 5),
+                             Products("Microcontroller Board", 34.95, 7),
+                             Products("Laser range finder", 149.99, 2),
+                             Products("Lithium polymer battery", 8.99,8)]
+    
      
 def main():
      cash = float(input("How much money do you have? $"))
@@ -43,4 +75,8 @@ def main():
                  print("Sorry, you cannot afford that product.")
          else:
              print("Sorry, we are sold out of", productNames[prodId])
- main()
+
+        
+     main()
+
+     
